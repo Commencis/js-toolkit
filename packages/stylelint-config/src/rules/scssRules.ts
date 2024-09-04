6b922c13cebc55166c8f5c9daf59d793cbf7c062
Mehmetcan Yılmaz <mehmetcan.yilmaz@commencis.com>
2024-09-05 00:37:31 +0300

    feat(stylelint-config): update variable spacing rule

--- packages/stylelint-config/src/rules/scssRules.ts
+++ packages/stylelint-config/src/rules/scssRules.ts
@@ -2,4 +2,14 @@ export const scssRules = {
   'scss/dollar-variable-no-namespaced-assignment': true,
   'scss/no-duplicate-dollar-variables': true,
   'scss/at-mixin-argumentless-call-parentheses': null,
+  'scss/dollar-variable-empty-line-before': [
+    'always',
+    {
+      ignore: [
+        'after-comment',
+        'inside-single-line-block',
+        'after-dollar-variable',
+      ],
+    },
+  ],
 };
