80fce8b7ac101ba7e5f8b8e66f7f21555218aefe
Mehmetcan Yılmaz <mehmetcan.yilmaz@commencis.com>
2024-09-11 22:56:01 +0300

    feat(eslint-plugin): add vue support to copyright-text rule

--- packages/eslint-plugin/src/utils/getCopyrightText.ts
+++ packages/eslint-plugin/src/utils/getCopyrightText.ts
@@ -1,9 +1,15 @@
 import { COPYRIGHT_TEXT } from '@/constants/copyright';
 
-export function getCopyrightText(startYear: number): string {
+export function getCopyrightText(startYear: number, isHtml: boolean): string {
   const currentYear = new Date().getFullYear();
-  return COPYRIGHT_TEXT.replace(/{startYear}/g, startYear.toString()).replace(
-    /{currentYear}/g,
-    currentYear.toString()
-  );
+
+  const formattedText = COPYRIGHT_TEXT.replace(
+    /{startYear}/g,
+    startYear.toString()
+  ).replace(/{currentYear}/g, currentYear.toString());
+
+  if (isHtml) {
+    return `<!--\n ${formattedText}\n-->`;
+  }
+  return `/*\n ${formattedText}\n */`;
 }
