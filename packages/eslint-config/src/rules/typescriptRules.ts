b5e99f59e0743ca5382d83fa8760ad0080b760d0
Unal Muslu <unal.muslu@commencis.com>
2024-09-26 09:49:51 +0300

    feat(eslint-config): add new react & typescript rules

--- packages/eslint-config/src/rules/typescriptRules.ts
+++ packages/eslint-config/src/rules/typescriptRules.ts
@@ -3,4 +3,7 @@ import { Linter } from '@typescript-eslint/utils/ts-eslint';
 export const typescriptRules: Linter.RulesRecord = {
   '@typescript-eslint/consistent-type-definitions': 'off',
   '@typescript-eslint/no-empty-function': 'off',
+  '@typescript-eslint/array-type': 'off',
+
+  '@typescript-eslint/explicit-function-return-type': 'error',
 };
