a8003671117d2798e461f9b274d46e261aa03fa8
Mehmetcan Yılmaz <mehmetcan.yilmaz@commencis.com>
2024-12-16 14:29:52 +0300

    feat(lint-staged-config): add preset exports

--- packages/lint-staged-config/src/index.ts
+++ packages/lint-staged-config/src/index.ts
@@ -1,4 +1,8 @@
 export * from './configs/base';
+export * from './configs/next';
+export * from './configs/react';
+export * from './configs/react-native';
 export * from './configs/typescript';
+export * from './configs/vue';
 export { GLOBS } from '@/constants';
 export { COMMANDS } from '@/helpers';
