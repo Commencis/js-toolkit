081453d3d297c85cf9edc37b21b87480d0125b5b
Mehmetcan Yılmaz <github@mcan.me>
2025-08-24 21:43:55 +0300

    feat(eslint-config): enable consistent type imports

--- packages/eslint-config/src/rules/typescriptRules.ts
+++ packages/eslint-config/src/rules/typescriptRules.ts
@@ -1,10 +1,11 @@
-import { Linter } from '@typescript-eslint/utils/ts-eslint';
+import type { Linter } from '@typescript-eslint/utils/ts-eslint';
 
 export const typescriptRules: Linter.RulesRecord = {
   '@typescript-eslint/consistent-type-definitions': 'off',
   '@typescript-eslint/no-empty-function': 'off',
   '@typescript-eslint/array-type': 'off',
 
+  '@typescript-eslint/consistent-type-imports': 'error',
   '@typescript-eslint/explicit-function-return-type': 'error',
   '@typescript-eslint/no-unused-vars': [
     'error',
